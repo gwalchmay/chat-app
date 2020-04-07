267261194ad6472d40214a9f097d0530fbe5ebd2
gwalchmay <l.luck@laposte.net>
2020-04-07 21:57:15 +0200

    transformed contact from a function to a class

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import ContactList from './components/ContactList';
+import Contact from './components/Contact';
 
 
 function App() {
   return (
     <div className="App">
-      <ContactList />
+      <Contact name='Robert Reyes' avatar='https://randomuser.me/api/portraits/men/28.jpg' />
+      <Contact name='Nellie Caldwell' avatar='https://randomuser.me/api/portraits/women/17.jpg' />
+      <Contact name='Vernon Mason' avatar='https://randomuser.me/api/portraits/men/84.jpg' />
+      <Contact name='Erica Hunt' avatar='https://randomuser.me/api/portraits/women/87.jpg' />
+      <Contact name='Juanita Philips' avatar='https://randomuser.me/api/portraits/women/55.jpg' />
     </div>
   );
 }
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,32 +1,42 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import './Contact.css'
+import './Contact.css';
 
-function Contact(props) {
-    return (
-        <div className="Contact">
-            <img
-                src= {props.avatar}
-                alt={props.name}
-                className="avatar"
-            />
-            <div>
-                <h4 className="name">{props.name}</h4>
-                <div className="status">
-                    <div className={props.online ? "status-online" : "status-offline"}></div>
-                    <p className="status-text">
-                        {props.online ? "Online" : "Offline"}
-                    </p>
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: false
+        };
+    }
+    render() {
+        return (
+            <div className="Contact">
+                <img
+                    src={this.props.avatar}
+                    alt={this.props.name}
+                    className="avatar"
+                />
+                <div className='status'>
+                    <h4 className="name">{this.props.name}</h4>
+                    <span className={this.state.online ? 'status-online' : 'status-offline'}>
+                        </span>
+                        <span onClick={event => {
+                            const newFavorite = !this.state.online;
+                            this.setState({ online: newFavorite })
+                        }}>
+                            {this.state.online ? 'online' : 'offline'}
+                    </span>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 Contact.propTypes = {
     avatar: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
-  };
+};
 
 
 export default Contact;
